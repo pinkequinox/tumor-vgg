a2fa28db83f1cc861bf23419655cd00930e7a3d2
pinkequinox <equinox.xsn@gmail.com>
2017-09-20 17:11:41 +0800

    remove vgg16 conv layer to classify tumor

---
+++ vgg16_modi3_ft.py
@@ -0,0 +1,349 @@
+# -*- coding: utf-8 -*-
+"""
+Transfer Learning tutorial
+==========================
+**Author**: `Sasank Chilamkurthy <https://chsasank.github.io>`_
+
+In this tutorial, you will learn how to train your network using
+transfer learning. You can read more about the transfer learning at `cs231n
+notes <http://cs231n.github.io/transfer-learning/>`__
+
+Quoting this notes,
+
+    In practice, very few people train an entire Convolutional Network
+    from scratch (with random initialization), because it is relatively
+    rare to have a dataset of sufficient size. Instead, it is common to
+    pretrain a ConvNet on a very large dataset (e.g. ImageNet, which
+    contains 1.2 million images with 1000 categories), and then use the
+    ConvNet either as an initialization or a fixed feature extractor for
+    the task of interest.
+
+These two major transfer learning scenarios looks as follows:
+
+-  **Finetuning the convnet**: Instead of random initializaion, we
+   initialize the network with a pretrained network, like the one that is
+   trained on imagenet 1000 dataset. Rest of the training looks as
+   usual.
+-  **ConvNet as fixed feature extractor**: Here, we will freeze the weights
+   for all of the network except that of the final fully connected
+   layer. This last fully connected layer is replaced with a new one
+   with random weights and only this layer is trained.
+
+"""
+# License: BSD
+# Author: Sasank Chilamkurthy
+
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import copy
+import os
+
+plt.ion()   # interactive mode
+
+######################################################################
+# Load Data
+# ---------
+#
+# We will use torchvision and torch.utils.data packages for loading the
+# data.
+#
+# The problem we're going to solve today is to train a model to classify
+# **ants** and **bees**. We have about 120 training images each for ants and bees.
+# There are 75 validation images for each class. Usually, this is a very
+# small dataset to generalize upon, if trained from scratch. Since we
+# are using transfer learning, we should be able to generalize reasonably
+# well.
+#
+# This dataset is a very small subset of imagenet.
+# 
+# .. Note ::
+#    Download the data from
+#    `here <https://download.pytorch.org/tutorial/hymenoptera_data.zip>`_
+#    and extract it to the current directory.
+
+# Data augmentation and normalization for training 
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+    	transforms.Scale((224, 224)),
+#        transforms.RandomSizedCrop(224),
+#        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Scale((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Scale((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = '/media/box/pytorch_classification_data'
+dsets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+         for x in ['train', 'val']}
+dtest = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+         for x in ['test']}
+dset_loaders = {x: torch.utils.data.DataLoader(dsets[x], batch_size=6,
+                                               shuffle=True, num_workers=4)
+                for x in ['train', 'val']}
+dtest_loaders = {x: torch.utils.data.DataLoader(dtest[x], batch_size=6,
+                                               shuffle=True, num_workers=0)
+                for x in ['test']}                
+dset_sizes = {x: len(dsets[x]) for x in ['train', 'val']}
+dtest_sizes = {x: len(dtest[x]) for x in ['test']}
+dset_classes = dsets['train'].classes
+
+use_gpu = torch.cuda.is_available()
+
+
+######################################################################
+# Visualize a few images
+# ^^^^^^^^^^^^^^^^^^^^^^
+# Let's visualize a few training images so as to understand the data
+# augmentations.
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+# Get a batch of training data
+inputs, classes = next(iter(dset_loaders['train']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+# imshow(out, title=[dset_classes[x] for x in classes])
+
+
+######################################################################
+# Training the model
+# ------------------
+#
+# Now, let's write a general function to train a model. Here, we will
+# illustrate:
+#
+# -  Scheduling the learning rate
+# -  Saving (deep copying) the best model
+#
+# In the following, parameter ``lr_scheduler(optimizer, epoch)``
+# is a function  which modifies ``optimizer`` so that the learning
+# rate is changed according to desired schedule.
+
+def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model = model
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                optimizer = lr_scheduler(optimizer, epoch)
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for data in dset_loaders[phase]:
+                # get the inputs
+                inputs, labels = data
+
+                # wrap them in Variable
+                if use_gpu:
+                    inputs, labels = Variable(inputs.cuda()), \
+                        Variable(labels.cuda())
+                else:
+                    inputs, labels = Variable(inputs), Variable(labels)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dset_sizes[phase]
+            epoch_acc = running_corrects / dset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model = copy.deepcopy(model)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    return best_model
+
+def test_model(model):
+
+
+    running_corrects = 0
+    for data in dtest_loaders['test']:
+	    # get the inputs
+	    inputs, labels = data
+
+	    # wrap them in Variable
+	    if use_gpu:
+	        inputs, labels = Variable(inputs.cuda()), \
+	            Variable(labels.cuda())
+	    else:
+	        inputs, labels = Variable(inputs), Variable(labels)
+
+	    outputs = model(inputs)
+	    _, preds = torch.max(outputs.data, 1)
+
+	    running_corrects += torch.sum(preds == labels.data)
+	    
+    acc = running_corrects / dtest_sizes['test']
+
+    print('Test Acc: {:.4f}'.format(acc))
+    return acc
+
+######################################################################
+# Learning rate scheduler
+# ^^^^^^^^^^^^^^^^^^^^^^^
+# Let's create our learning rate scheduler. We will exponentially
+# decrease the learning rate once every few epochs.
+
+def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
+    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
+
+    if epoch % lr_decay_epoch == 0:
+        print('LR is set to {}'.format(lr))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    return optimizer
+
+
+######################################################################
+# Visualizing the model predictions
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Generic function to display predictions for a few images
+#
+
+def visualize_model(model, num_images=6):
+    images_so_far = 0
+    fig = plt.figure()
+
+    for i, data in enumerate(dset_loaders['val']):
+        inputs, labels = data
+        if use_gpu:
+            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(inputs), Variable(labels)
+
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+
+        for j in range(inputs.size()[0]):
+            images_so_far += 1
+            ax = plt.subplot(num_images//2, 2, images_so_far)
+            ax.axis('off')
+            ax.set_title('predicted: {}'.format(dset_classes[preds[j]]))
+            imshow(inputs.cpu().data[j])
+
+            if images_so_far == num_images:
+                return
+
+######################################################################
+# Finetuning the convnet
+# ----------------------
+#
+# Load a pretrained model and reset final fully connected layer.
+#
+
+model_ft = models.vgg16(pretrained=True)
+
+mod_f = list(model_ft.features.children())
+#for popidx in range(30, 22, -1):
+for popidx in range(30, 15, -1):
+    mod_f.pop(popidx)
+mod_f.append(nn.MaxPool2d(kernel_size=8, stride=8))
+new_features = nn.Sequential(*mod_f)
+model_ft.features = new_features
+
+mod = list(model_ft.classifier.children())
+mod.pop()
+mod.append(nn.Linear(4096, 2))
+mod.pop(0)
+mod.insert(0, nn.Linear(12544, 4096))
+new_classifier = nn.Sequential(*mod)
+model_ft.classifier = new_classifier
+
+if use_gpu:
+    model_ft = model_ft.cuda()
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+######################################################################
+# Train and evaluate
+# ^^^^^^^^^^^^^^^^^^
+#
+# It should take around 15-25 min on CPU. On GPU though, it takes less than a
+# minute.
+#
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=15)
+test_accu = test_model(model_ft)
+######################################################################
+#
+
+# visualize_model(model_ft)
+
+
+plt.ioff()
+plt.show()
